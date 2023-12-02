284b9c7e7f7eb740f5780cecc03af4e3534f04e4
het98 <hetalsonagara98@gmail.com>
2023-12-02 19:29:24 +0530

    first commit

---
+++ app/Http/Controllers/ResizeImageController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Image;
+
+class ResizeImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        return view('image.display');
+     
+    
+    }
+
+    public function resizeImage(Request $request){
+        $this->validate($request, [
+            'file' => 'required|image|mimes:jpg,jpeg,png,gif,svg|max:2048',
+        ]);
+        $image = $request->file('file');
+        $input['file'] = time().'.'.$image->getClientOriginalExtension();
+        
+        $destinationPath = public_path('/productImage');
+        $imgFile = Image::make($image->getRealPath());
+        $imgFile->resize(275, 200, function ($constraint) {
+		    $constraint->aspectRatio();
+		})->save($destinationPath.'/'.$input['file']);
+        $destinationPath = public_path('/uploads');
+        $image->move($destinationPath, $input['file']);
+        return back()
+        	->with('success','Image has successfully uploaded.')
+        	->with('fileName',$input['file']);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
